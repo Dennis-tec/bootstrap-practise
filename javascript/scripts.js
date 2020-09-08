eaf874d368e0dbeb2894dc163b3d548bce3a1807
Dennis Yiaile <ykamakei6345@gmail.com>
2020-09-08 15:26:20 +0300

    addition and subtraction credentials

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,3 +4,9 @@ return number1/number2;
 var number1=parseInt(prompt("enter a number:"));
 var number2=parseInt(prompt("enter other number:"));
 alert(add(number1, number2));
+var subtract=function(number1, number2){
+  return number1*number2;
+};
+var number1=prompt("enter a number:");
+var number2=prompt("enter another number:");
+alert(subtract(number1, number2));
