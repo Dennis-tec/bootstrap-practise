d3db8eafde746ad1e72597310df4877427b742c0
Dennis Yiaile <ykamakei6345@gmail.com>
2020-09-09 11:55:03 +0300

    add image and css styles float styles

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,3 +10,9 @@ var subtract=function(number1, number2){
 var number1=prompt("enter a number:");
 var number2=prompt("enter another number:");
 alert(subtract(number1, number2));
+var animals=true;
+if (false) {
+  console.log("do not carry a camera");
+}else {
+  console.console.log("carry camera");
+}
