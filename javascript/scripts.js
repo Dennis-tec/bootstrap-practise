dbba450b8caf84b2a59479e258805e4514cb10d0
Dennis Yiaile <ykamakei6345@gmail.com>
2020-09-09 13:26:33 +0300

    add images and boirder styles

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,18 +1,2 @@
-var add=function(number1, number2){
-return number1/number2;
-};
-var number1=parseInt(prompt("enter a number:"));
-var number2=parseInt(prompt("enter other number:"));
-alert(add(number1, number2));
-var subtract=function(number1, number2){
-  return number1*number2;
-};
-var number1=prompt("enter a number:");
-var number2=prompt("enter another number:");
-alert(subtract(number1, number2));
-var animals=true;
-if (false) {
-  console.log("do not carry a camera");
-}else {
-  console.console.log("carry camera");
+
 }
